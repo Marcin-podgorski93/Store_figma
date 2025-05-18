347e5ecf49d4ba5f16932e003dd1ca600e235d21
marcinpodgorski <podgorski.marcin93@gmail.com>
2025-05-18 23:01:26 +0200

    create top bar from figma

---
+++ front-end/src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import { Footer } from "../Footer/Footer";
+import { Logo } from "../Logo/Logo";
+import { MainMenu } from "../MainMenu/MainMenu";
+
+export function Layout() {
+  return (
+    <>
+      <MainMenu />
+      <Logo />
+      <Footer />
+    </>
+  );
+}
