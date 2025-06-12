856c4b67a4c33d328a00d5c92531277a5855b8e4
marcinpodgorski <podgorski.marcin93@gmail.com>
2025-06-13 00:31:16 +0200

    Refactor Breadcrumbs component to use dynamic routing for gender and category; implement subcategory handling.

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,35 +1,49 @@
 import styles from "./Breadcrumbs.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
+import { CATEGORIES, GENDERS } from "../../constants/categories";
 
 export function Breadcrumbs() {
+  const { gender, category, subcategory } = useParams();
+  console.log(gender, category, subcategory);
+
+  const foundGender = GENDERS.find((g) => g.path === gender);
+  const foundCategory = CATEGORIES.find((c) => c.path === category);
+
   const breadcrumbs = [
     {
-      categoryName: "Kobieta",
-      path: "kobieta",
+      categoryName: foundGender.categoryName,
+      path: `/${foundGender.path}`,
     },
     {
-      categoryName: "Odzież",
-      path: "odziez",
-    },
-    {
-      categoryName: "Swetry",
-      path: "swetry",
+      categoryName: foundCategory.categoryName,
+      path: `/${foundGender.path}/${foundCategory.path}`,
     },
   ];
 
-  return (
-    <ul className={styles.breadcrumbs}>
-      {breadcrumbs.map((breadcrumb) => {
-        return (
-          <li key={breadcrumb.path}>
-            <NavLink to={breadcrumb.path}>
-              {breadcrumb.categoryName}
-              <img src={ARROW_ICON} />
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  if (subcategory) {
+    const foundSubcategory = foundCategory.subcategories.find(
+      (sc) => sc.path === subcategory
+    );
+
+    breadcrumbs.push({
+      categoryName: foundSubcategory.categoryName,
+      path: `/${foundGender.path}/${foundCategory.path}/${foundSubcategory.path}`,
+    });
+
+    return (
+      <ul className={styles.breadcrumbs}>
+        {breadcrumbs.map((breadcrumb) => {
+          return (
+            <li key={breadcrumb.path}>
+              <NavLink end to={breadcrumb.path}>
+                {breadcrumb.categoryName}
+                <img src={ARROW_ICON} />
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
