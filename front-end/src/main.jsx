e84538f7f5df522e1ed7b0941559385a8b83e081
marcinpodgorski <podgorski.marcin93@gmail.com>
2025-06-12 23:57:17 +0200

    Refactor category navigation and product loading: update CategoryMenu and ExpandableMenu to use dynamic routing with gender and category; implement productListLoader for fetching products based on selected filters.

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,6 +8,8 @@ import { Favourites } from "./views/Favourites/Favourites";
 import { Layout } from "./components/Layout/Layout";
 import { MainPage } from "./views/MainPage/MainPage";
 import { mainPageLoader } from "./api/mainPageLoader";
+import { productListLoader } from "./api/productListLoader";
+import { ProductsList } from "./views/ProductsList/ProductsList";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +28,11 @@ const router = createBrowserRouter([
         path: "/:gender?",
         element: <MainPage />,
         loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
       },
     ],
   },
